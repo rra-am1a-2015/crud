ddc811c8f96198fe97f8f1f9e9389a2515fecd2c
Arjan de Ruijter <rra@mboutrecht.nl>
2015-09-21 11:11:39 +0200

    array

--- read_personalia.php
+++ read_personalia.php
@@ -12,8 +12,25 @@
 	// SELECT-query's geven altijd een resultaat terug.
 	$result = mysqli_query($connection, $query);
 	
+	
 	// Studenten verteld over bestaan github.com.
-	var_dump($result);
+	//var_dump($result);
+	
+	$row = mysqli_fetch_array($result);
+	
+	// Wat is een array
+	$fruit1 = "appel";
+	$fruit2 = "mandarijn";
+	$fruit3 = "peer";
+	
+	echo "Ik heb wat fruit gekocht, namelijk: ".$fruit1.", ".$fruit2.", ".$fruit3."<br>";
+
+	$fruit = array("peer", "appel", "mandarijn");
+	
+	echo "Het gezondst is, in aflopende volgorde: ".$fruit[2].", ".$fruit[1].", ".$fruit[0];
+	
+	// Opdracht: maak een array auto, sla daarin 5 automerken op en geef deze weer in een
+	// orderered list <ol><li></li></ol>. Lievelingsmerk bovenaan...
 	
 	
 	
