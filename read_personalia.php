f1d14453bb3bbe7e1f8790536d7d89129bde3138
Arjan de Ruijter <rra@mboutrecht.nl>
2015-09-21 10:53:26 +0200

    Vardump

--- read_personalia.php
+++ read_personalia.php
@@ -6,6 +6,14 @@
 	
 	$connection = mysqli_connect($servername, $username, $password, $dbname) or die("Fout geen server-verbinding".mysqli_error($connection));
 	
+	$query = "SELECT * FROM `personalia`";
+	
+	// Sla wat je terugkrijgt van de functie mysqli_query op in de variabele $result.
+	// SELECT-query's geven altijd een resultaat terug.
+	$result = mysqli_query($connection, $query);
+	
+	// Studenten verteld over bestaan github.com.
+	var_dump($result);
 	
 	
 	
