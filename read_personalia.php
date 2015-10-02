9144df859514a929f2404cb0c598afdf246b04ad
Arjan de Ruijter <rra@mboutrecht.nl>
2015-10-02 09:31:36 +0200

    update form met submit knop

--- read_personalia.php
+++ read_personalia.php
@@ -1,10 +1,5 @@
 <?php
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "am1a_inlogregistratie";
-	
-	$connection = mysqli_connect($servername, $username, $password, $dbname) or die("Fout geen server-verbinding".mysqli_error($connection));
+	require("db_connect.php");
 	
 	$query = "SELECT * FROM `personalia`";
 	
--- update_personalia.php
+++ update_personalia.php
@@ -1,5 +1,5 @@
 <?php
-	echo $_GET["id"];
+	//echo $_GET["id"];
 	include("db_connect.php");
 	//include_once("db_connect.php");
 	//require("db_connect.php");
@@ -22,9 +22,53 @@
 	</head>
 	<body>
 		<form action="update_personalia.php" method="post">
-			voornaam: <input type="text"
-							 name="voornaam"
-							 value="<?php echo $record["voornaam"]; ?>">
+			<table>
+				<tr>
+					<td>voornaam:</td>
+					<td>
+						<input type="text"
+							   name="voornaam"
+							   value="<?php echo $record["voornaam"]; ?>">
+					</td>
+				</tr>
+				<tr>
+					<td>tussenvoegsel:</td>
+					<td><input type="text"
+							   name="tussenvoegsel"
+							   value="<?php echo $record["tussenvoegsel"] ?>">
+					</td>
+				</tr>
+				<tr>
+					<td>achternaam:</td>
+					<td><input type="text"
+							   name="achternaam"
+							   value="<?php echo $record["achternaam"] ?>">
+					</td>
+				</tr>
+				<tr>
+					<td>e-mail:</td>
+					<td><input type="email"
+							   name="email"
+							   value="<?php echo $record["email"] ?>">
+					</td>
+				</tr>
+				<tr>
+					<td>haarkleur:</td>
+					<td><input type="text"
+							   name="haarkleur"
+							   value="<?php echo $record["haarkleur"] ?>">
+					</td>
+				</tr>
+				<tr>
+					<td></td>
+					<td><input type="submit"
+							   name="submit"
+							   value="wijzig gegevens">
+					</td>
+				</tr>
+			
+			 
+			
 		</form>
 	</body>
 </html>
